1562268bf5f35c82372270d7708a54db2c35161a
Victor <vgomez@avathartech.com>
2023-12-28 16:37:38 -0400

    Step 01 - Quick Introduction to REST - Understand Resource and Actions Step 02 - Creating Spring Boot Project for REST with Maven and Eclipse Step 03 - Creating Your First Spring Boot Resource - Hello World Step 04 - Creating a Second Spring Boot Resource Method - Hello World Bean Step 05 - Exploring Path Params and Path Variables with Spring Boot

---
+++ src/main/java/com/victorgomez/springboot/springsurveyrestapi/SpringSurveyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.victorgomez.springboot.springsurveyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSurveyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSurveyRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorgomez/springboot/springsurveyrestapi/SpringSurveyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorgomez.springboot.springsurveyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSurveyRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
